db94a914e5832bf5a696d72fdeccca8a6a5d6b9d
codegarden13 <code@xviz.de>
2025-04-29 13:04:41 +0200

    added POC: 'theme scaffolding'

---
+++ wp-scaffold-template.sh
@@ -0,0 +1,5 @@
+
+docker-compose run --user 33:33 --rm wpcli_stg --path=stg scaffold _s sample-theme \
+  --theme_name="codegarden13" \
+  --author="codegarden13" \
+  --author_uri="https://xviz.de"
